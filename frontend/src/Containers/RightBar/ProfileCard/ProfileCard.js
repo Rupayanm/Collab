c4e522cde269b557cd3ed1950555513b3c287f6d
Subhajit Mandal <smandal0511@gmail.com>
2022-05-19 20:29:51 +0530

    Optional Chain ProfileCard

--- frontend/src/Containers/RightBar/ProfileCard/ProfileCard.js
+++ frontend/src/Containers/RightBar/ProfileCard/ProfileCard.js
@@ -28,27 +28,27 @@ const ProfileCard = () => {
 
   return (
     <>
-      <div className=" m-2 rounded-xl border border-gray-300 scrollbar-hide">
-        <div className="py-4 h-32 flex items-center relative">
+      <div className="m-2 border border-gray-300  rounded-xl scrollbar-hide">
+        <div className="relative flex items-center h-32 py-4">
           <div className="rounded-xl ml-3 z-10 h-[5.5rem] w-28  overflow-hidden">
             <img
               src={profileData?.avatar}
               alt="profile"
-              className="rounded-r-2xl h-24 w-24 "
+              className="w-24 h-24 rounded-r-2xl "
             ></img>
           </div>
-          <div className="h-full w-full z-10 flex flex-col justify-center">
-            <div className="text-xl flex items-end h-1/2 px-3  font-medium">
+          <div className="z-10 flex flex-col justify-center w-full h-full">
+            <div className="flex items-end px-3 text-xl font-medium h-1/2">
               {profileData?.name}
             </div>
-            <div className=" px-3 h-1/2 text-purple-600 font-medium break-words">
+            <div className="px-3 font-medium text-purple-600 break-words  h-1/2">
               {profileData?.designation || profileData?.email}
             </div>
           </div>
-          <div className="h-1/2 w-full bg-gray-200 z-0 absolute bottom-0"></div>
+          <div className="absolute bottom-0 z-0 w-full bg-gray-200 h-1/2"></div>
         </div>
-        <div className="text-sm p-5">{profileData?.about}</div>
-        <div className="flex flex-row flex-nowrap justify-center divide-x-2 divide-gray-400 mx-4 mb-6">
+        <div className="p-5 text-sm">{profileData?.about}</div>
+        <div className="flex flex-row justify-center mx-4 mb-6 divide-x-2 divide-gray-400 flex-nowrap">
           <div className="px-10 text-center">
             <h6 className="text-lg font-bold text-deep-purple-accent-400">
               {profileData?.post || 0}
@@ -63,8 +63,8 @@ const ProfileCard = () => {
           </div>
         </div>
         {profileData?.socials && (
-          <div className="flex justify-evenly m-6">
-            {socialList.map((item, index) => (
+          <div className="flex m-6 justify-evenly">
+            {socialList?.map((item, index) => (
               <button
                 key={index}
                 className={`border text-gray-800 rounded-full p-2 border-gray-300 cursor-pointer hover:border-gray-500 hover:text-${item.name}`}
@@ -74,11 +74,11 @@ const ProfileCard = () => {
             ))}
           </div>
         )}
-        <div className="m-5 flex flex-wrap justify-center gap-2">
-          {profileData?.skills.map((item, index) => (
+        <div className="flex flex-wrap justify-center gap-2 m-5">
+          {profileData?.skills?.map((item, index) => (
             <div
               key={index}
-              className="font-medium text-sm w-min py-1 px-3 rounded-full border"
+              className="px-3 py-1 text-sm font-medium border rounded-full w-min"
             >
               {item}
             </div>
