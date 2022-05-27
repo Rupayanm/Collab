b4d8c8f09e2e20eae0d2ef3ed4d1a47ed071c00f
Subhajit Mandal <smandal0511@gmail.com>
2022-05-27 12:07:32 +0530

    Serve Static files

--- server.js
+++ server.js
@@ -42,4 +42,14 @@ app.use("/api/upload", require("./routes/api/upload"));
 
 app.get("/", (req, res) => res.send("API Running"));
 
-app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("frontend/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
+const HOST = "0.0.0.0";
+app.listen(PORT, HOST, () => console.log(`server started on port ${PORT}`));
